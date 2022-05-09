5e6e39ef3d7bc34af7b6e7c7b23d25683cda822b
David <davidweese15@gmail.com>
2022-05-08 21:54:20 -0500

    COMPLETE PAPER ROCK SCISSORS GAME
    added a prompt to choose player selection.  Added if function to compare player and computer selections to determine winner.

--- prs.js
+++ prs.js
@@ -7,4 +7,33 @@ let paperRockScissors = ['paper', 'rock', 'scissors'];
 function computerPlay() {
     return paperRockScissors[Math.floor(Math.random()*paperRockScissors.length)];
 }
-console.log(computerPlay());
+
+let playerSelection = prompt("Do you want to choose rock, paper, or scissors?");
+let computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playerSelection);
+let x = playerSelection.toLowerCase();
+let y = computerSelection.toLowerCase();
+function game() {
+    
+    
+    
+
+   if (x===y) {
+       return "draw!";
+   
+    }else if (x === "rock" && y === "scissors") {
+       return "you win!";
+    
+    }else if (x === "scissors" && y ==="paper") {
+        return "you win!";
+    }else if (x === "paper" && y === "rock") {
+        return 'you win!';
+    
+    }else {
+        return "You lose!";
+    }
+
+    }
+console.log(game());
+
