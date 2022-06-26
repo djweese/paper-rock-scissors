c9a86838ecf1bba42561147fe675996d3e7d387c
David <davidweese15@gmail.com>
2022-06-26 10:20:34 -0500

    RPS UI MESS

--- prs.js
+++ prs.js
@@ -15,9 +15,37 @@ let computerScore = 0;
 
 //create one round of paper rock scissors
 //changed this function to keep score rather than declare a winner
+
+const buttons = document.querySelectorAll('button');
+const rockBtn=document.getElementById('#rock');
+const paperBtn=document.getElementById('#paper');
+const scissorsBtn=document.getElementById('#scissors');
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click',(e) => {
+       playerSelection=e.target.id;
+       playRound();
+       
+       console.log(playerScore);
+       console.log(computerScore);
+    });
+    
+});
+const cs = document.querySelector('#computerScore');
+const ps = document.querySelector('#playerScore');
+function winner() {
+    
+    if (playerScore == 5) {
+        results.textContent = "The player wins!";
+    }else if (computerScore == 5) {
+        results.textContent = "The computer wins!";
+    }
+}
 function playRound() {
-    let playerSelection = prompt("Do you want to choose rock, paper, or scissors?");
-    let x = playerSelection.toLowerCase();
+   
+    console.log(playerSelection);
+    let x = playerSelection;
     let computerSelection = computerPlay();
     let y = computerSelection.toLowerCase();
    
@@ -36,31 +64,56 @@ function playRound() {
     }else {
        computerScore +=1;
     }
-    
+
+for (i=0; i<6; i++) {
+    cs.textContent = "Computer: " + computerScore;
+}
+
+for (i=0; i<6; i++) {
+    ps.textContent = "Player: " + playerScore;
+}
+winner();
+
     }
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+}
+function gameEnd() {
+    if (playerScore >=5 || computerScore >=5) {
+        reset();
+    }
+}
+gameEnd();
+//fix this.  score always starts with the condition in the if statement.
+
+
+//need to create a div and keep track of score in it
+
+
 
 
 //we've created a function for a round.  Now we need to create a best of five game
 //make a loop to run the game five times
 //need to keep score of those games
 //and the score needs to be used to declare a winner.
-function game() {
+//function game() {
 //loop
-    for (i=0; i < 5; i++) {
-        playRound();
-    }
+//    for (i=0; i < 5; i++) {
+//      playRound();
+//   }
 //declare winner
-        function score() {
-            if (playerScore > computerScore) {
-                return "You are the winner!";
-            }else if (playerScore < computerScore) {
-                return "The computer is the winner!"
-            }else {
-                return "DRAW!";
-            }
-        }
-    return score();  
-}
-console.log(game());
-console.log(playerScore);
-console.log(computerScore);
+//        function score() {
+//            if (playerScore > computerScore) {
+  //              return "You are the winner!";
+    //        }else if (playerScore < computerScore) {
+      //          return "The computer is the winner!"
+        //    }else {
+          //      return "DRAW!";
+            //}
+      //  }
+  //  return score();  
+//}
+//console.log(game());
+//console.log(playerScore);
+//console.log(computerScore);
